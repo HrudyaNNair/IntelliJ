e365916a131e5db1dd6a549c47bef68f46ef5464
HrudyaNNair <hrudyannair@gmail.com>
2024-12-29 11:01:06 +0530

    first commit

--- Demo.java
+++ Demo.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
 public class Demo {
+    public static void main(String[] Args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++) {
+            System.out.println("Enter element:");
+            arr[i] = sc.nextInt();
+        }
+        int m = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int product = arr[i]* arr[j];
+                for(int k =0; k<n;k++) {
+                    if (arr[k]==product) {
+                        if(product>m)
+                            m = product;
+                    }
+                }
+            }
+        }
+        System.out.println(m);
+
+
+
+    }
+
 }
