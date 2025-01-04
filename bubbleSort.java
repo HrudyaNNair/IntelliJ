b6a251b69f6ab7169d8e67576d47a63dc69169bd
HrudyaNNair <hrudyannair@gmail.com>
2025-01-04 23:30:59 +0530

    first commit

--- bubbleSort.java
+++ bubbleSort.java
@@ -4,7 +4,7 @@ public class bubbleSort {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size: ");
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        int[] arr = new int[n];
         for(int i =0;i<n;i++){
             System.out.println("Enter element: ");
             arr[i] = sc.nextInt();
